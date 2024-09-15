719755a07ae71d06e1ff387a43883afec3541d36
Julio Rarick <juliorarick@gmail.com>
2024-09-15 05:38:23 -0300

    ➕ add stack based in array, class with methods

---
+++ src/data-structures/stack-array.ts
@@ -0,0 +1,40 @@
+interface StackContract<ElementType> {
+  push(element: ElementType): void
+  pop(): ElementType | undefined
+  peek(): ElementType | undefined
+  isEmpty(): boolean
+  size(): number
+  clear(): void
+}
+
+export class Stack<ElementType> implements StackContract<ElementType> {
+  private items: ElementType[]
+
+  constructor() {
+    this.items = []
+  }
+
+  push(element: ElementType): void {
+    this.items.push(element)
+  }
+
+  pop() {
+    return this.items.pop()
+  }
+
+  peek() {
+    return this.items[this.items.length - 1]
+  }
+
+  isEmpty() {
+    return this.items.length === 0
+  }
+
+  size() {
+    return this.items.length
+  }
+
+  clear() {
+    this.items = []
+  }
+}
